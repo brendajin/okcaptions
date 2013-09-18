81370755fd11c0efb4c424b3612449c199e27f83
brendajin <brendajin@gmail.com>
2013-09-18 01:10:07 -0700

    added var caption json for transcript:

--- static/js/script.js
+++ static/js/script.js
@@ -27,3 +27,32 @@ function onYouTubePlayerReady(playerId) {
   ytplayer.addEventListener("onStateChange", "onPlayerStateChange");
   ytplayer.addEventListener("onError", "onPlayerError");
 }
+
+var caption = {
+  "13": "Hold your breath Make a wish",
+  "15": "Count to three",
+  "17":"Come with me And you'll be",
+  "21": "In a world of pure imagination",
+  "26":"Take a look and you'll see Into your imagination",
+  "38":"We'll begin with a spin",
+  "43":"Travelling in the world of my creation",
+  "47":"What we'll see will defy explanation",
+  "64":"If you want to view paradise",
+  "67":"Simply look around and view it",
+  "71":"Anything you want to, do it",
+  "76":"Wanna change the world?",
+  "80":"There's nothing to it",
+  "101":"There is no life i know",
+  "104":"To compare with pure imagination",
+  "107":"Living there you'll be free",
+  "110":"If you truly wish to be",
+  "155":"If you want to view paradise",
+  "159":"Simply look around and view",
+  "163":"Anything you want to do it",
+  "167":"Wanna change the world",
+  "170":"There's nothing to it",
+  "185":"There is no life i know",
+  "191":"To compare with pure imagination",
+  "197":"Living there you'll be free",
+  "204":"If you truly wish to be"
+  }
